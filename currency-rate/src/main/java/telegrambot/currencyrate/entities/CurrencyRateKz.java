9fcc8ed66f65edea849446209adf99b4c18de0da
MrKoshkin <Kosh.job@gmail.com>
2024-03-01 13:57:58 +0500

    Added bot menu and bot reply

--- currency-rate/src/main/java/telegrambot/currencyrate/entities/CurrencyRateKz.java
+++ currency-rate/src/main/java/telegrambot/currencyrate/entities/CurrencyRateKz.java
@@ -2,6 +2,12 @@ package telegrambot.currencyrate.entities;
 
 import lombok.Data;
 
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 @Data
 public class CurrencyRateKz {
     private String date;
@@ -9,13 +15,17 @@ public class CurrencyRateKz {
     private double value;
     private String change;
 
+    private final static DecimalFormat df = new DecimalFormat("#.##");
+    private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public void setDate(String date) {
+        LocalDate localDate = LocalDate.parse(date, formatter);
+        String monthName = localDate.getMonth().getDisplayName(TextStyle.FULL, new Locale("ru"));
+        this.date = localDate.getDayOfMonth() + " " + monthName.toLowerCase();
+    }
+
     @Override
     public String toString() {
-        return "CurrencyRateKz{" +
-                "date='" + date + '\'' +
-                ", charCode='" + charCode + '\'' +
-                ", value=" + value +
-                ", change='" + change + '\'' +
-                '}';
+        return "Курс " + charCode + "/KZT на " + date + " " + df.format(value) + " тенге";
     }
 }
--- currency-rate/src/main/java/telegrambot/currencyrate/entities/CurrencyRateRus.java
+++ currency-rate/src/main/java/telegrambot/currencyrate/entities/CurrencyRateRus.java
@@ -2,6 +2,11 @@ package telegrambot.currencyrate.entities;
 
 import lombok.Data;
 
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 @Data
 public class CurrencyRateRus {
@@ -11,20 +16,19 @@ public class CurrencyRateRus {
     private String name;
     private double value;
 
+    private final static DecimalFormat df = new DecimalFormat("#.##");
+    private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+
     public static void setDate(String date) {
-        CurrencyRateRus.date = date;
+        LocalDate localDate = LocalDate.parse(date, formatter);
+        String monthName = localDate.getMonth().getDisplayName(TextStyle.FULL, new Locale("ru"));
+        CurrencyRateRus.date = localDate.getDayOfMonth() + " " + monthName.toLowerCase();
     }
 
-    public static String getDate() {
-        return date;
-    }
 
     @Override
     public String toString() {
-        return "CurrencyRateRus{" +
-                "charCode='" + charCode + '\'' +
-                ", name='" + name + '\'' +
-                ", value=" + value +
-                '}';
+        return "Курс " + charCode + "/RUB на " + date + " " + df.format(value) + " руб.";
     }
 }
